45169f8e215531cdf42ac20a9bc037c2adcfff4d
Khaled Yousef <khaledyousef196@gmail.com>
2024-03-20 08:17:25 +0200

    Added Libraries GLFW, GLEW, GLM

---
+++ ThirdParty/GLEW/Build-GLEW.lua
@@ -0,0 +1,39 @@
+project "GLEW"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "Binaries/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   files { "include/GL/glew.h", "src/glew.c" }
+
+   includedirs
+   {
+       "include"
+   }
+
+   links { "OpenGL32" }
+
+   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { "GLEW_STATIC" }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
---
+++ ThirdParty/GLFW/Build-GLFW.lua
@@ -0,0 +1,37 @@
+project "GLFW"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "Binaries/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   files { "include/**.h", "src/**.c" }
+
+   includedirs
+   {
+       "include"
+   }
+
+   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { "_GLFW_WIN32" }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
